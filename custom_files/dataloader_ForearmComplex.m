ee31819370d02aba9fc49a43cd2bfdd4497dd1a7
Roberto Scardigno <12681493+roberto722@users.noreply.github.com>
2026-02-25 12:14:32 +0100

    Refactor multispectral dataloaders to reduce duplication

--- custom_files/dataloader_ForearmComplex.m
+++ custom_files/dataloader_ForearmComplex.m
@@ -7,20 +7,36 @@ function [sinograms, file_names] = dataloader_ForearmComplex(ids, model)
 sino_folder = 'E:\Scardigno\Fotoacustica-MB\data\prove_x_BA\lead step12mm width25mm\mat files\';
 
 if isempty(ids)
-    D_images = dir([sino_folder '/*.mat']);
-    ids = struct2cell(D_images);
-    ids = ids(1, :)';
+    D_images = dir(fullfile(sino_folder, '*.mat'));
+    ids = {D_images.name}';
 end
+
+wavelengths = {'750', '800', '850'};
+field_names = { ...
+    'RF_sottocampionato_31p25MHz_750', ...
+    'RF_sottocampionato_31p25MHz_800', ...
+    'RF_sottocampionato_31p25MHz_850' ...
+};
+
+num_sinograms = numel(ids) * numel(wavelengths);
+file_names = cell(1, num_sinograms);
+sinograms = [];
+out_idx = 1;
 
 for i = 1:numel(ids)
-    file_ = load(strcat(sino_folder, ids{i}));
-    sinograms(:, :, 1+3*(i-1)) = check_length_ForearmComplex(file_.RF_sottocampionato_31p25MHz_750, model); 
-    sinograms(:, :, 2+3*(i-1)) = check_length_ForearmComplex(file_.RF_sottocampionato_31p25MHz_800, model); 
-    sinograms(:, :, 3+3*(i-1)) = check_length_ForearmComplex(file_.RF_sottocampionato_31p25MHz_850, model);
+    file_ = load(fullfile(sino_folder, ids{i}));
 
-    file_names{1+3*(i-1)} = strcat(ids{i}(1:end-4), '_750');
-    file_names{2+3*(i-1)} = strcat(ids{i}(1:end-4), '_800');
-    file_names{3+3*(i-1)} = strcat(ids{i}(1:end-4), '_850');
+    for w = 1:numel(wavelengths)
+        checked_signal = check_length_ForearmComplex(file_.(field_names{w}), model);
+
+        if isempty(sinograms)
+            sinograms = zeros(size(checked_signal, 1), size(checked_signal, 2), num_sinograms, 'like', checked_signal);
+        end
+
+        sinograms(:, :, out_idx) = checked_signal;
+        [basename, ~, ~] = fileparts(ids{i});
+        file_names{out_idx} = strcat(basename, '_', wavelengths{w});
+        out_idx = out_idx + 1;
+    end
     i
 end
-
--- custom_files/dataloader_HDF5_subsampler.m
+++ custom_files/dataloader_HDF5_subsampler.m
@@ -1,23 +1,35 @@
-function [sinograms, file_names] = dataloader_HDF5_submsampler(ids, model)
+function [sinograms, file_names] = dataloader_HDF5_subsampler(ids, model)
 
-%sino_folder = 'E:\Scardigno\Fotoacustica-MB\data\Forearm2000\'; 
+%sino_folder = 'E:\Scardigno\Fotoacustica-MB\data\Forearm2000\';
 sino_folder = 'E:\Scardigno\Fotoacustica-MB\data\Forearm_complex_HDF5_samples\transverse\';
 
 if isempty(ids)
-    D_images = dir([sino_folder '/*.hdf5']);
-    ids = struct2cell(D_images);
-    ids = ids(1, :)';
+    D_images = dir(fullfile(sino_folder, '*.hdf5'));
+    ids = {D_images.name}';
 end
+
+wavelengths = {'750', '800', '850'};
+num_sinograms = numel(ids) * numel(wavelengths);
+file_names = cell(1, num_sinograms);
+sinograms = [];
+out_idx = 1;
 
 for i = 1:numel(ids)
-    sinograms(:, :, 1+3*(i-1)) = subSampler(check_length_HDF5_subsampler(h5read(strcat(sino_folder, ids{i}), '/simulations/time_series_data/750'), model));
-    sinograms(:, :, 2+3*(i-1)) = subSampler(check_length_HDF5_subsampler(h5read(strcat(sino_folder, ids{i}), '/simulations/time_series_data/800'), model));
-    sinograms(:, :, 3+3*(i-1)) = subSampler(check_length_HDF5_subsampler(h5read(strcat(sino_folder, ids{i}), '/simulations/time_series_data/850'), model));
+    for w = 1:numel(wavelengths)
+        wavelength = wavelengths{w};
+        path_hdf5 = fullfile(sino_folder, ids{i});
+        raw_signal = h5read(path_hdf5, ['/simulations/time_series_data/' wavelength]);
+        checked_signal = check_length_HDF5_subsampler(raw_signal, model);
+        checked_signal = subSampler(checked_signal);
 
-    file_names{1+3*(i-1)} = strcat(ids{i}(1:end-5), '_750');
-    file_names{2+3*(i-1)} = strcat(ids{i}(1:end-5), '_800');
-    file_names{3+3*(i-1)} = strcat(ids{i}(1:end-5), '_850');
+        if isempty(sinograms)
+            sinograms = zeros(size(checked_signal, 1), size(checked_signal, 2), num_sinograms, 'like', checked_signal);
+        end
+
+        sinograms(:, :, out_idx) = checked_signal;
+        [basename, ~, ~] = fileparts(ids{i});
+        file_names{out_idx} = strcat(basename, '_', wavelength);
+        out_idx = out_idx + 1;
+    end
     i
 end
-
-
